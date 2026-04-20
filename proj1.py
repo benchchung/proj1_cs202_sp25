e3a38e3fcfabeb51f13089fb861e4e88277db779
benchchung <bchung28@calpoly.edu>
2026-04-20 14:21:07 -0700

    implemented attributes

--- proj1.py
+++ proj1.py
@@ -1,14 +1,25 @@
 #complete your tasks in this file
 from dataclasses import dataclass
+from typing import Any, TypeAlias
+
+degrees:TypeAlias = float
 
 @dataclass(frozen = True)
 class GlobeRect:
-    pass
+    lo_lat:degrees
+    hi_lat:degrees
+    west_long:degrees
+    east_long:degrees
 
 @dataclass(frozen = True)
 class Region:
-    pass
+    rect:GlobeRect
+    name:str
+    terrain:str
 
 @dataclass(frozen = True)
 class RegionCondition:
-    pass
+    region:Region
+    year:int
+    pop:int
+    ghg_rate:float
